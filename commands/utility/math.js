9da7400e378923da420e54d8f91c967174d195b8
Shiroe2324 <93495611+Shiroe2324@users.noreply.github.com>
2023-01-05 22:03:44 -0500

    Add math.js

---
+++ commands/utility/math.js
@@ -0,0 +1,39 @@
+const { PermissionFlagsBits, Message, Client, EmbedBuilder } = require('discord.js');
+const Utils = require('../../utils');
+
+/**
+ * @property name - The name of the command.
+ * @property usage - The syntax in which the command is used.
+ * @property aliases - The aliases of the command.
+ * @property cooldown - the cooldown time of the command
+ * @property category - The name of the command category.
+ * @property description - The description of the command.
+ * @property onlyCreator - Check if the command is only for the creator of the bot.
+ * @property botPermissions - List of bot permissions for the command.
+ * @property userPermissions - List of user permissions for the command.
+ */
+module.exports = {
+    name: 'math',
+    usage: 'math [expresión]',
+    aliases: ['calc', 'c'],
+    cooldown: 2000,
+    category: 'utilidad',
+    description: 'Calcula una expresión matemática y devuelve su resultado',
+    onlyCreator: false,
+    botPermissions: [
+        PermissionFlagsBits.ViewChannel,
+        PermissionFlagsBits.SendMessages,
+        PermissionFlagsBits.EmbedLinks
+    ],
+    userPermissions: [],
+
+    /**
+     * function with the code to execute the command.
+     * @param {Message} msg - The message sent by the user.
+     * @param {string[]} args - The arguments of the message sent by the user.
+     * @param {Client} client - The bot's client.
+     */
+    execute: async (msg, args, client) => {
+        
+    }
+}
